5afb26cd94234f0c998649c569265d4fa8aa1dc6
Faizou <faizouaremou@gmail.com>
2022-12-07 02:16:28 +0100

    correct webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
-
-module.exports = {
-    mode: 'development',
-    entry: './src/types.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_module/,
-            include: [
-                path.resolve(__dirname, "src")
-            ],
-        }, ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+const path = require("path");
+const config = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_module/,
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
+module.exports = config;
